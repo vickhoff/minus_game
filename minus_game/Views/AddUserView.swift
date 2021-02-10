8fe9881958bd01414b3a9f2b6be7efbf6f7eee3b
vickhoff <dvickhoff@gmail.com>
2021-02-11 00:45:19 +0100

    Add custom users :D

---
+++ minus_game/Views/AddUserView.swift
@@ -0,0 +1,20 @@
+//
+//  AddUserView.swift
+//  minus_game
+//
+//  Created by David Vickhoff on 2021-02-10.
+//
+
+import SwiftUI
+
+struct AddUserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddUserView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddUserView()
+    }
+}
